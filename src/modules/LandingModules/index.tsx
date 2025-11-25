cd077f92f70f7b81f82fcfbf9b5a33af7808775a
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-11-25 19:52:10 +0700

    about us

--- src/modules/LandingModules/index.tsx
+++ src/modules/LandingModules/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "./section/Hero";
+import About from "./section/About";
 
 const LandingModules = () => {
   return (
     <main className="flex flex-col">
       <Hero />
+      <About />
     </main>
   );
 };
