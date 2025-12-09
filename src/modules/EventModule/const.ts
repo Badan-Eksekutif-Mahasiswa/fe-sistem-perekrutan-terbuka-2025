908715c61a506586259262a4a7c7dbbd3b9ce60f
VazhaK14 <khayrivazhasocial@gmail.com>
2025-12-09 10:08:17 +0700

    feat: timeline section

--- src/modules/EventModule/const.ts
+++ src/modules/EventModule/const.ts
@@ -29,4 +29,39 @@ export const timelineData = [
     title: "Pendaftaran Batch 1",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
   },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
+  {
+    date: new Date(),
+    title: "Pendaftaran Batch 1",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis fuga quaerat corporis voluptatem eos architecto, ut dignissimos sed, amet laboriosam, dolorum itaque? Officia, quaerat architecto. Tempora laudantium esse officia animi.",
+  },
 ];
--- src/modules/EventModule/sections/TimelineSection.tsx
+++ src/modules/EventModule/sections/TimelineSection.tsx
@@ -12,12 +12,12 @@ const TimelineCard = ({ date, title, desc }: EventTimelineType) => {
     day: "numeric",
   });
   return (
-    <div className="flex flex-col text-white gap-4 items-center text-center max-w-[324px] ">
-      <Button className=" text-m2 px-6 py-2 rounded-full">
+    <div className="flex flex-col text-white gap-4 items-center text-center">
+      <Button className=" text-m2 max-md:text-m3 px-6 py-2 rounded-full">
         {formattedDate}
       </Button>
-      <h2 className=" text-h2 ">{title}</h2>
-      <p className="text-p5">{desc}</p>
+      <h2 className=" text-h2 max-md:text-h5">{title}</h2>
+      <p className="text-p5 max-md:text-p6">{desc}</p>
     </div>
   );
 };
@@ -26,7 +26,7 @@ const TimelineMarker = ({ isEven, isLast }: TimelineMarkerProps) => {
   return (
     <div className="w-full flex justify-center items-center relative ">
       {!isLast && (
-        <div className="absolute mt-2 top-1/2 left-1/2 w-full rounded-full h-1.5 bg-white -translate-y-1/2 -z-10" />
+        <div className="absolute mt-2 top-1/2 left-1/2 w-[91%] mx-auto rounded-full h-1.5 bg-white ml-4 -translate-y-1/2 -z-10" />
       )}
 
       <div className={` ${!isEven ? "rotate-180 mt-10" : "mb-2"}`}>
@@ -38,43 +38,46 @@ const TimelineMarker = ({ isEven, isLast }: TimelineMarkerProps) => {
 
 const TimelineSection = () => {
   return (
-    <section className="w-full min-h-screen bg-primary-500 flex flex-col justify-center items-center py-20 overflow-x-auto">
-      <div className="relative min-w-[900px] px-10">
-        <div className="flex justify-between items-stretch w-full relative z-10">
-          {timelineData.map((item, index) => {
-            const isEven = index % 2 === 0;
-            const isLast = index === timelineData.length - 1;
+    <>
+      <h1 className="text-center text-h1 text-white mb-5">Timeline</h1>
+      <main className="w-full min-h-screen bg-primary-500 flex flex-col gap-10 py-10  overflow-x-auto">
+        <div className="relative w-max px-10 md:px-20 md:pr-40 ">
+          <div className="flex justify-between items-stretch w-full relative z-10">
+            {timelineData.map((item, index) => {
+              const isEven = index % 2 === 0;
+              const isLast = index === timelineData.length - 1;
 
-            return (
-              <div
-                key={index}
-                className={`flex flex-1 flex-col items-center ${
-                  isEven ? "justify-start" : "justify-end"
-                }`}
-              >
+              return (
                 <div
-                  className={`flex-1 flex items-end pb-8 ${
-                    !isEven ? "invisible" : ""
+                  key={index}
+                  className={`flex shrink-0 w-[350px] flex-col items-center ${
+                    isEven ? "justify-start" : "justify-end"
                   }`}
                 >
-                  <TimelineCard {...item} />
-                </div>
+                  <div
+                    className={`flex-1 flex items-end pb-8 ${
+                      !isEven ? "invisible" : ""
+                    }`}
+                  >
+                    <TimelineCard {...item} />
+                  </div>
 
-                <TimelineMarker isEven={isEven} isLast={isLast} />
+                  <TimelineMarker isEven={isEven} isLast={isLast} />
 
-                <div
-                  className={`flex-1 flex items-start pt-8 ${
-                    isEven ? "invisible" : ""
-                  }`}
-                >
-                  <TimelineCard {...item} />
+                  <div
+                    className={`flex-1 flex items-start pt-8 ${
+                      isEven ? "invisible" : ""
+                    } `}
+                  >
+                    <TimelineCard {...item} />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </main>
+    </>
   );
 };
 
